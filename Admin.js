190bebdefc9cc21776728fabb972a6c040021eaa
Jovan Koledin <jovank2001@gmail.com>
2023-09-15 18:10:21 -0700

    Adding initial Admin class

---
+++ Admin.js
@@ -0,0 +1,85 @@
+class Admin {
+  constructor(username, password) {
+    if (!username || !password) {
+      throw new Error("Both username and password are required");
+    }
+    this.username = username;
+    this.password = password; // Note: Password should be hashed and not stored in plaintext
+    this.accessLevel = "admin";
+  }
+
+  // Method to authenticate the admin user
+  authenticate() {
+    // Logic to authenticate user
+  }
+
+  // CRUD Operations for patient accounts
+  
+  // Create new patient account
+  createPatientAccount(patientInfo) {
+    if (!patientInfo || typeof patientInfo !== "object") {
+      throw new Error("Invalid patient information");
+    }
+    // Logic to create a new patient account
+  }
+
+  // Read patient account information
+  readPatientAccount(patientId) {
+    if (!patientId) {
+      throw new Error("Patient ID is required");
+    }
+    // Logic to read patient information
+  }
+
+  // Update patient account information
+  updatePatientAccount(patientId, updatedInfo) {
+    if (!patientId || !updatedInfo || typeof updatedInfo !== "object") {
+      throw new Error("Invalid input parameters");
+    }
+    // Logic to update patient information
+  }
+
+  // Delete patient account
+  deletePatientAccount(patientId) {
+    if (!patientId) {
+      throw new Error("Patient ID is required");
+    }
+    // Logic to delete a patient account
+  }
+
+  // Method to manage staff accounts
+  
+  // Create new staff account
+  createStaffAccount(staffInfo) {
+    if (!staffInfo || typeof staffInfo !== "object") {
+      throw new Error("Invalid staff information");
+    }
+    // Logic to create a new staff account
+  }
+
+  // Update staff account information
+  updateStaffAccount(staffId, updatedInfo) {
+    if (!staffId || !updatedInfo || typeof updatedInfo !== "object") {
+      throw new Error("Invalid input parameters");
+    }
+    // Logic to update staff account information
+  }
+
+  // Delete staff account
+  deleteStaffAccount(staffId) {
+    if (!staffId) {
+      throw new Error("Staff ID is required");
+    }
+    // Logic to delete a staff account
+  }
+
+  // Audit Logging
+  
+  // Log an action performed by admin
+  logAction(action, details) {
+    if (!action || !details) {
+      throw new Error("Action and details are required for auditing");
+    }
+    // Logic to log admin actions for auditing
+  }
+}
